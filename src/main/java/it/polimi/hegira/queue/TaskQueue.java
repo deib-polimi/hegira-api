95fdd5b933db2a5106a7199b70120680d167701b
Marco <scavuzzo.marco@gmail.com>
2015-07-28 13:36:21 +0200

    Hegira-api has the ability to purge the task queue.

---
+++ src/main/java/it/polimi/hegira/queue/TaskQueue.java
@@ -0,0 +1,139 @@
+package it.polimi.hegira.queue;
+
+import it.polimi.hegira.exceptions.QueueException;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
+import com.rabbitmq.client.AMQP.Queue.PurgeOk;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
+
+/**
+ * TaskQueue class used exclusively to purge the task queue.
+ *  
+ * @author Marco Scavuzzo
+ */
+public class TaskQueue {
+	private static Logger log = Logger.getLogger(TaskQueue.class);
+
+	private ConnectionFactory factory;
+	private Connection connection;
+	private Channel channel;
+	private QueueingConsumer consumer;
+	protected static final String TASK_QUEUE_NAME = "task_queue";
+
+	/**
+	 * Creates a task queue between the SRC and the TWC.
+	 * @param queueAddress The address where the RabbitMQ broker is deployed. Default: localhost
+	 * @throws QueueException If a connection cannot be established.
+	 */
+	public TaskQueue(String queueAddress) throws QueueException{
+		
+		factory = new ConnectionFactory();
+		if(queueAddress==null || queueAddress.isEmpty()){
+			factory.setHost("localhost");
+		}else{
+			factory.setHost(queueAddress);
+		}
+		
+		try {
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			/**
+			 * Declaring a durable queue
+			 * queueDeclare(java.lang.String queue, boolean durable, 
+			 * boolean exclusive, boolean autoDelete, 
+			 * java.util.Map<java.lang.String,java.lang.Object> arguments) 
+			 */
+			channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+			channel.basicQos(1);
+			consumer = new QueueingConsumer(channel);
+			/**
+			 * basicConsume(java.lang.String queue, boolean autoAck, Consumer callback)
+			 * Starts a non-nolocal, non-exclusive consumer, 
+			 * with a server-generated consumerTag.
+			 */
+			channel.basicConsume(TASK_QUEUE_NAME, false, consumer);
+					
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new QueueException(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Acknowledge a message given its delivery (returned from the QueuingConsumer object).
+	 * @param delivery
+	 * @throws QueueException
+	 */
+	public void sendAck(Delivery delivery) throws QueueException{
+		try {
+			channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+		} catch (IOException e) {
+			throw new QueueException(e.getMessage(),e.getCause());
+		}
+	}
+	
+	/**
+	 * In case a message cannot be processed properly a negative acknowledgment must be sent.
+	 * @param delivery The message that was not processed.
+	 * @throws QueueException
+	 */
+	public void sendNack(Delivery delivery) throws QueueException{
+		try {
+			/**
+			 * void basicNack(long deliveryTag,
+             *					boolean multiple,
+             *					boolean requeue)
+			 */
+			channel.basicNack(delivery.getEnvelope().getDeliveryTag(),
+					false, true);
+			/**
+			 * void basicReject(long deliveryTag,
+             *					boolean requeue)
+			 */
+			channel.basicReject(delivery.getEnvelope().getDeliveryTag(), true);
+		} catch (IOException e) {
+			throw new QueueException(e.getMessage(),e.getCause());
+		}
+	}
+	
+	/**
+	 * Purges the task queue.
+	 * @return <b>true</b> if purged; <b>false</b> otherwise.
+	 * @throws IOException
+	 * @throws ShutdownSignalException
+	 * @throws ConsumerCancelledException
+	 * @throws InterruptedException
+	 * @throws QueueException
+	 */
+	public boolean purgeQueue() throws IOException, ShutdownSignalException, ConsumerCancelledException, InterruptedException, QueueException{
+		PurgeOk queuePurge = channel.queuePurge(TASK_QUEUE_NAME);
+		if(queuePurge!=null){
+			Delivery delivery = consumer.nextDelivery(50);
+			if(delivery!=null){
+				sendAck(delivery);
+				log.debug(Thread.currentThread().getName()+
+						" consumed orphan");
+			}
+		}
+		return (queuePurge != null) ?  true :  false;
+	}
+	
+	/**
+	 * Disconnects the client from the task queue closing the channel and the connection.
+	 * @throws IOException
+	 */
+	public void disconnect() throws IOException{
+		if(channel!=null) channel.close();
+		if(connection!=null) connection.close();
+	}
+}
